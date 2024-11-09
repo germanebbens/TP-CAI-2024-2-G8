cec9511c9776f4d4041ab7d738843cf17cfe48ba
German Ebbens <german.ebbens@clipdashboard.com>
2024-11-09 10:50:14 -0300

    initial commit - template provided

---
+++ TemplateTPIntegrador/Negocio/LoginNegocio.cs
@@ -0,0 +1,83 @@
+﻿using Datos;
+using Persistencia;
+using Persistencia.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class LoginNegocio
+    {
+        public String login(String usuario, String password) {
+            String perfilLogin = "";
+
+            // Paso 1: Registrar intentos
+            LoginDB loginDB = new LoginDB();
+            int cantidadIntentos = loginDB.obtenerIntentos(usuario);
+
+            if (cantidadIntentos == 0)
+            {
+                loginDB.guardarIntento(usuario);
+            }
+            else if(cantidadIntentos <= 3)
+            {
+                int valorActualizado = cantidadIntentos + 1;
+                loginDB.actualizarIntento(usuario, valorActualizado.ToString());
+            }
+            else
+            {
+                // circuito de bloqueado de usuario
+            }
+
+            // Paso 2: Llamar login
+            LoginWS loginWS = new LoginWS();   
+            String idUsuario = loginWS.login(usuario, password);
+
+            // Paso 2.1: Verificar primer login
+            verificarPrimerLogin(usuario);
+
+            // Paso 2.2: Credenciales invalidas
+           
+            // Paso 2.3: Credenciales validas
+
+            // Pase 3: Obtener perfil del usuario logueado
+            List<UsuarioWS> usuariosActivos = loginWS.buscarDatosUsuario();
+
+            // Paseo 3.1: Verificar que el usuario este activo
+
+            // Paso 3.2: Buscar el id en el listado de Usuarios activos
+            int perfilUsuarioLogueado = 0;
+
+            foreach(UsuarioWS usuarioActivo in usuariosActivos)
+            {
+                if (usuarioActivo.Id.Equals(idUsuario))
+                {
+                    perfilUsuarioLogueado = usuarioActivo.Perfil;
+                }
+            }
+
+            // Paso 4: Mandar al formulario que corresponde
+            if(perfilUsuarioLogueado == 3)
+            {
+                perfilLogin = "Administrador";
+            } else if (perfilUsuarioLogueado == 2)
+            {
+                perfilLogin = "Supervisor";
+            }
+            else
+            {
+                perfilLogin = "Vendedor";
+            }
+
+            return perfilLogin;
+        }
+
+        private void verificarPrimerLogin(string usuario)
+        {
+            // A desarrollar por X
+        }
+    }
+}
---
+++ TemplateTPIntegrador/TemplateTPIntegrador/Form1.cs
@@ -0,0 +1,48 @@
+﻿using Negocio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TemplateTPIntegrador
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            String usuario = txtUsuario.Text;
+            String password = txtPassword.Text;
+
+            try
+            {
+                LoginNegocio loginNegocio = new LoginNegocio();
+                loginNegocio.login(usuario, password);
+
+                Form form = new InitForm();
+                this.Hide();
+                form.ShowDialog();
+
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
